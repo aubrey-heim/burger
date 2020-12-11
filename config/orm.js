7e376e146d98a759f20f76274ac5beb36a500b87
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-10 16:49:19 -0800

    added selectAll functionality

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,16 @@
 const connection = require("./connection.js");
+
+const orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });},
+    insertOne: function (){},
+    updateOne: function (){}
+}
+
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js")
 
-module.exports = router;
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        
+        toEatData = data.filter(burger => burger.devoured === 0)
+        eatenData = data.filter(burger => burger.devoured === 1)
+
+      res.render("index", { toEat: toEatData, eaten: eatenData });
+    });
+  });
+  
+  module.exports = router;
